7325c767b5db81cd7e31a657b55479989c22439a
asaschajer <aurore.schajer@outlook.fr>
2026-01-13 14:13:12 +0100

    change the script for it to compare all cities

---
+++ script_all_cities.py
@@ -0,0 +1,69 @@
+import json
+
+precipitation = {}
+
+station_codes = ['GHCND:USW00093814','GHCND:US1WAKG0038','GHCND:USC00513317','GHCND:US1CASD0032']
+
+for station in station_codes:
+    with open('precipitation.json') as file:
+        data = json.load(file)
+        
+        #Filtering to keep only the data from seattle
+        station_data = []
+        
+        for measurement in data:
+            if measurement['station'] == station:
+                station_data.append(measurement)
+        # print(seattle_data)
+
+
+        splitted_date = []
+        for measurement in station_data:
+            measurement['date'].split('-')[1]
+            split_by_month = measurement['date'].split('-')[1]
+
+            new_dic={
+                'month': split_by_month,
+                'value': measurement['value']
+            }
+            splitted_date.append(new_dic)
+
+        # print(splitted_date)
+        
+        # calculate the total monthly precipitation 
+        total_monthly_precipitation = {}
+        for value in splitted_date:
+            month = value['month']
+            if month not in total_monthly_precipitation:
+                total_monthly_precipitation[month] = 0
+                total_monthly_precipitation[month] += value['value']
+            else:
+                total_monthly_precipitation[month] += value['value']
+        print(total_monthly_precipitation)
+
+        # calculate the total yearly precipitation
+        values_list = list(total_monthly_precipitation.values())
+        total_yearly_precipitation = 0
+        for monthly_precipitation in values_list:
+            total_yearly_precipitation = total_yearly_precipitation + monthly_precipitation
+        print(total_yearly_precipitation)
+
+        #calculate the relative monthly precipitation
+        relative_monthly_precipitation = {}
+        for month in total_monthly_precipitation:
+            result_relative_monthly_pre = total_monthly_precipitation[month]/total_yearly_precipitation
+            relative_monthly_precipitation[month] = result_relative_monthly_pre
+        print(relative_monthly_precipitation)
+
+
+        import json   
+        precipitation['Seattle'] = {
+            'station': station_codes,
+            'state': 'WA',
+            'total_monthly_precipitation': total_monthly_precipitation,
+            'relative_monthly_precipitation': relative_monthly_precipitation
+        }
+
+#write the result output
+with open('results_seattle.json', 'w', encoding='utf-8') as file:
+    json.dump(precipitation, file, indent=4)
